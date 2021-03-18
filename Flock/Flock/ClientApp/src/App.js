0321c32648d275d67dbd14b77475e78b12af1182
Stefanos Toufexis <stefetoufe@gmail.com>
2021-03-18 15:22:46 +0200

    Project ready to start development
    Fixed inherit vulnerabilities in default template, cleaned up the file system from all the default stuff and created a basic "Hello world" app.
    Note: Backend still contains one cs file called weather forecast. I left it there so the back end folks can take a look and maybe find a few hints as to how they are supposed to work within this environment

--- Flock/Flock/ClientApp/src/App.js
+++ Flock/Flock/ClientApp/src/App.js
@@ -1,22 +1,12 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React from 'react';
 
-import './custom.css'
 
-export default class App extends Component {
-  static displayName = App.name;
+const App = () => {
 
-  render () {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+        <h1>Hello world!!!!!!!</h1>
+        );
+
 }
+
+export default App;
