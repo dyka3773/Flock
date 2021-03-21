19daa444b18e006361722f7772a30d703e2200c2
dyka3773 <63119280+dyka3773@users.noreply.github.com>
2021-03-21 13:49:15 +0200

    I have no idea what I'm doing honestly
    I'm trying to follow an online tutorial on how to connect Firebase with C# in ASP.NET, but I have no idea how anything is going to be used by C#

---
+++ Flock/Flock/Controllers/DBConnection.cs
@@ -0,0 +1,24 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flock.Controllers
+{
+    public class DBConnection
+    {
+        private string collectionName;
+        public FirestoreDb fireStoreDb;
+
+        public DBConnection(string CollectionName)
+        {
+            string filepath = "./flock-adopse-firebase-adminsdk-f7byo-29221649c9.json";
+
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", filepath);
+
+            fireStoreDb = FirestoreDb.Create("what-is-this");
+            collectionName = CollectionName;
+        }
+    }
+}
