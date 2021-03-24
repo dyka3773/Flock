1517b2e912e0a313b19330e926a146a2d4f6a8ec
dyka3773 <63119280+dyka3773@users.noreply.github.com>
2021-03-24 20:06:59 +0200

    Created the connection with the db class ***
    Haven't tested this but I think it works

--- Flock/Flock/Controllers/DBConnection.cs
+++ Flock/Flock/Controllers/DBConnection.cs
@@ -1,24 +1,51 @@
-﻿using Google.Cloud.Firestore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Flock.Controllers
 {
     public class DBConnection
     {
-        private string collectionName;
-        public FirestoreDb fireStoreDb;
+        private string myConnection;
 
-        public DBConnection(string CollectionName)
+        private MySqlConnection connection;
+        //private MySqlConnectionStringBuilder properties;
+
+        public DBConnection()
         {
-            string filepath = "./flock-adopse-firebase-adminsdk-f7byo-29221649c9.json";
+            myConnection = "SERVER=localhost;Port=3333;DATABASE=Library;UID=root;PASSWORD=dyka3773";
+            //Until we deploy the site in users, we have to connect through ssh with the db !!!
+            //ssh -L3333:/home/student/it/2018/it185200/mysql/run/mysql.sock it185200@users.iee.ihu.gr
+            //password : It185200@197358426
+        }
 
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", filepath);
+        public MySqlConnection connect()
+        {
+            if (connection == null)
+            {
+                try
+                {
+                    connection = new MySqlConnection(myConnection);
+                   
+                }
+                catch (Exception) { throw; }
+            }
+            return connection;
+        }
 
-            fireStoreDb = FirestoreDb.Create("what-is-this");
-            collectionName = CollectionName;
+        public void disconnect()
+        {
+            if(connection != null)
+            {
+                try
+                {
+                    connection.Close();
+                    connection = null;
+                }
+                catch (Exception) { throw; }
+            }
         }
     }
 }
