a3cb8f4dd29bc14ecc6551c3c14e26d220f524d9
Stefanos Toufexis <stefetoufe@gmail.com>
2021-04-16 13:42:05 +0300

    Finished up the ImportContacts Component

--- Flock/Flock/ClientApp/src/components/ImportContacts.js
+++ Flock/Flock/ClientApp/src/components/ImportContacts.js
@@ -6,38 +6,52 @@ import getGroups from '../dataRequests/getGroups';
 
 const ImportContacts = () => {
 
-    const [selectedGroup, setSelectedGroup] = useState(0);
+    const [selectedGroupId, setSelectedGroupId] = useState("-1");
+
+    const defaultGroup = {id:"none",name:"None"};
 
     const groupsData = getGroups();
+    groupsData.push(defaultGroup);
 
     const groups = groupsData.map((group) => {
         return <option value={group.id} key={group.id}>{group.name}</option>
     });
 
     const createContacts = (e) => {
-        e.preventDefault();
 
-
-        if (document.querySelector("#import").files[0])
-            csvToJson(document.querySelector("#import").files[0], groupsData[selectedGroup].id);
-        else
+        if (!document.querySelector("#import").files[0]){
             window.alert("Please upload a csv");
+            e.preventDefault()
+            return;
+        }
 
+        if (selectedGroupId === "-1") {
+            window.alert("Please select a group");
+            e.preventDefault()
+            return;
+        }
+
+        csvToJson(document.querySelector("#import").files[0], selectedGroupId);
+        window.alert("Success");
     }
+
+
+
+
 
     return <div className="import-contacts">
 
         <form onSubmit={createContacts}>
            
                 <label className="custom-file-upload">
-                <input type="file" id="import" />
-                Upload Icon
-            </label>
+                    <input type="file" id="import" />
+                    Upload CSV
+                </label>
             
 
             
-                <select className="ui selection dropdown" value={selectedGroup} onChange={(e) => setSelectedGroup(e.target.value)} >
-                   
+                <select className="ui selection dropdown" value={selectedGroupId} onChange={(e) => setSelectedGroupId(e.target.value)} required>
+                    <option value="-1" disabled>Select a group</option>
                     {groups}
                 </select>
             
