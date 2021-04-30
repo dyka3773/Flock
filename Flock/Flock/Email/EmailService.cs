63d190f39921cb5d5694f991820fa91f0370aa27
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2021-04-30 18:35:55 +0300

    small refactor
    -removed empty lines
    -moved methods into new functions
    -some basic documentation
    -tidied mailsender()

--- Flock/Flock/Email/EmailService.cs
+++ Flock/Flock/Email/EmailService.cs
@@ -32,57 +32,58 @@ namespace SendEmail
             contacts = new GroupsController().GetContacts(camp.AID, camp.GID);
 
         }
-        
-
-        
-        
 
         public void mailSender()
         {
-
             foreach (Contact c in contacts)
             {
                 Debug.WriteLine(c.ToString());
-                string address = getUserName();
-                string password = getPass();
-                SmtpClient client = new SmtpClient("something.com", 587);
-                client.UseDefaultCredentials = false;
-                client.Credentials = new System.Net.NetworkCredential(address, password);
-                client.EnableSsl = false;
-                client.DeliveryMethod = SmtpDeliveryMethod.Network;
-
                 
-
-                MailMessage mailMessage = new MailMessage();
-                mailMessage.From = new MailAddress(getUserName());
-
-                mailMessage.To.Add(c.email);
-
-
-                mailMessage.Body = camp.text;
-                mailMessage.Subject = camp.subject;
-                customSend(client, mailMessage);
-                
+                customSend(userClient(c),mail(c));
             }
-
         }
+        //uses contacts (list object) field to send a predeterminded message to the specified adresses 
 
         public async void customSend(SmtpClient client,MailMessage mailMessage) {
 
             Random rnd = new Random();
             await client.SendMailAsync(mailMessage);
-            
         }
+        //sends asynchronously a message with a fixed client
 
+        public MailMessage mail(Contact c) 
+        {
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(getUserName());
+            mailMessage.To.Add(c.email);
+            mailMessage.Body = camp.text;
+            mailMessage.Subject = camp.subject;
+            return mailMessage;
+        }
+        //creates a mail message based on the recipient 
+        //each time this method is called a new message is constructed with fixed sender and content but the recipient varies
+
+        public SmtpClient userClient(Contact c) 
+        {
+            SmtpClient client = new SmtpClient("something.com", 587);
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential( getUserName(), getPass() );
+            client.EnableSsl = false;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            return client;
+        }
+        //creates a client based on the users credentials
 
         public string getUserName()
         {
             return "imanemail@something.com";
         }
+        //gets the adress of the sender
+
         public string getPass()
         {
             return "852456";
         }
- 
+        //gets the password of the sender
     }
 }
