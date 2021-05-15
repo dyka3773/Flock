2642adb6080b05d7f118131704acb05f614dd55b
Capta1n-Ner0 <61006930+Capta1n-Ner0@users.noreply.github.com>
2021-05-15 13:16:14 +0300

    Icons and Homepage Text

--- Flock/Flock/ClientApp/src/modules/Homepage.js
+++ Flock/Flock/ClientApp/src/modules/Homepage.js
@@ -2,6 +2,12 @@
 
 import logo from "../images/logo.png";
 
+import personal from "../images/Personal.png";
+
+import business from "../images/Business.png";
+
+import company from "../images/Company.png";
+
 import '../modulesCSS/Homepage.css';
 
 const Homepage = () => {
@@ -19,24 +25,27 @@ const Homepage = () => {
                     <div className="five wide column">
                         <div className="ui segment">
                             <h2>
-                                Personal use
+                            Personal use
                         </h2>
+                        <img src={personal} className="ui centered medium circular image" />
                         </div>
                     </div>
 
                     <div className="five wide column">
                         <div className="ui segment">
                             <h2>
-                                Business use
+                            Business use
                         </h2>
+                        <img src={business} className="ui centered medium circular image" />
                         </div>
                     </div>
 
                     <div className="five wide column">
                         <div className="ui segment">
                             <h2>
-                                Company use
+                            Company use
                         </h2>
+                        <img src={company} className="ui centered medium circular image" />
                         </div>
                     </div>
 
@@ -49,22 +58,21 @@ const Homepage = () => {
                         <i className="right triangle icon"></i>
                         <div className="content">
                             <h3 className="header">Personal use</h3>
-                            <div className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                            </div>
+                        <div className="description">If you ever wanted to send something to your friends, but you are bored to do it for each and every one of them, then Flock has you covered for it!</div>
                         </div>
                     </a>
                     <a className="item">
                         <i className="right triangle icon"></i>
                         <div className="content">
                             <h3 className="header">Business use</h3>
-                            <div className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+                        <div className="description">Freelancing or having your own website might be hard promoting your campaigns. With Flock, you can easily send your campaigns to your subscribers and tell them about everything!</div>
                         </div>
                     </a>
                     <a className="item">
                         <i className="right triangle icon"></i>
                         <div className="content">
                             <h3 className="header">Company use</h3>
-                            <div className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+                        <div className="description">The hardest thing for a boss is to remember all the names of his employees, left aside all of their emails. With Flock, their job informing that you are fired will be easier!</div>
                         </div>
                     </a>
                 </div>
@@ -75,14 +83,20 @@ const Homepage = () => {
                 <div className="ui  list">
                     <a className="item">
                         <div className="content">
-                            <h3 className="header">Alpha</h3>
-                            <div className="description">Alpha is the first letter</div>
+                            <h3 className="header">It is User Friendly</h3>
+                            <div className="description">Flock is made in a way where every person can use it without having to struggle. Everything is in front of the user, making his expirience much more enjoyable</div>
                         </div>
                     </a>
                     <a className="item">
                         <div className="content">
-                            <h3 className="header">Beta</h3>
-                            <div className="description">Beta is the second letter</div>
+                            <h3 className="header">It support Markdown</h3>
+                            <div className="description">Some people love to use custom made texts, like heading or bold. Flock is having you covered. Every campaign that we use support Markdown, making customization a playful job for the user</div>
+                        </div>
+                    </a>
+                    <a className="item">
+                        <div className="content">
+                            <h3 className="header">The coloring</h3>
+                        <div className="description">Plenty of users have problem with bright colors, or using Dark Mode instead. Flock's colors are chosen in a way that the eyes will be protected from the brightness, thus working with our site is not eye tiredness</div>
                         </div>
                     </a>
                 </div>
